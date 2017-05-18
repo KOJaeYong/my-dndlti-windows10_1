c00e57788d9a4548ea9a81fe69344bfb2e15c323
DNDLTIHE\dndltihe <mhanaro728@naver.com>
2017-05-18 19:17:36 +0900

    질문/답변 QnA 관련 html 파일 추가

--- src/main/java/net/dndlti/web/WelcomeController.java
+++ src/main/java/net/dndlti/web/WelcomeController.java
@@ -1,5 +1,9 @@
 package net.dndlti.web;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 	
 	//get 방식으로 url mapping
+	//http://localhost:8080/helloworld?name=dndlti&age=30
 	@GetMapping("/helloworld")
 	public String welcome(String name, 
 	int age, Model model) {
@@ -16,6 +21,30 @@ public class WelcomeController {
 		model.addAttribute("name", name);
 		model.addAttribute("age", age);
 		return "welcome";
+	}
+	
+	@GetMapping("/helloworld_1")
+	public String welcome_1(Model model) {
+		model.addAttribute("name", "manidto");
+		model.addAttribute("value", 10000);
+		model.addAttribute("taxed_value:", 10000-(10000*0.4));
+		model.addAttribute("in_ca", true);
+		return "welcome_1";
+	}
+	
+	@GetMapping("/helloworld_2")
+	public String welcome_2(Model model) {
+		model.addAttribute("name", "manidto");
+		model.addAttribute("company", "<b>Dndlti</b>");
+		return "welcome_2";
+	}
+	
+	@GetMapping("helloworld_3")
+	public String welcome_3(Model model) {
+		/*List<MyModel> repo = new ArrayList<MyModel>();*/
+		List<MyModel> repo = Arrays.asList(new MyModel("우이샤"), new MyModel("마이또"));
+		model.addAttribute("repo", repo);
+		return "welcome_3";
 	}
 	
 	
