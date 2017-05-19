0402b25d079e6ac8193f802f652f61743bde9bd9
DNDLTIHE\dndltihe <mhanaro728@naver.com>
2017-05-20 01:33:52 +0900

    새로운 HTML 적용 및 URL 리팩토링(원본파일 삭제) - 2 => 전체적인 url mapping 중복 방지를 위해 수정

--- src/main/java/net/dndlti/web/UserController.java
+++ src/main/java/net/dndlti/web/UserController.java
@@ -11,6 +11,7 @@ import net.dndlti.domain.User;
 import net.dndlti.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
 	/*회원가입 저장*/
@@ -23,7 +24,7 @@ public class UserController {
 	private UserRepository userRepository;
 	
 	//http://localhost:8080/user/form.html에서 회원가입
-	@PostMapping("/users")	
+	@PostMapping("")	
 	public String memberCreate(User user) {
 		System.out.println("user: " + user);
 		/*users.add(user);*/
@@ -32,7 +33,7 @@ public class UserController {
 		return "redirect:/users";
 	}
 	//@RequestMapping 어노테이션도 사용가능
-	@GetMapping("/users")
+	@GetMapping("")
 	public String memberList(Model model) {
 		/*model.addAttribute("users", users);*/
 		//H2 데이터베이스 User 테이블에 있는 레코드를 
@@ -41,29 +42,4 @@ public class UserController {
 		userRepository.findAll());
 		return "/user/list";
 	}
-	
-	@PostMapping("/create")	
-	public String create(User user) {
-		System.out.println("user: " + user);
-		
-		/*users.add(user);*/
-		userRepository.save(user);
-		
-		/*@RequestMapping("/list") list 메소드 호출*/
-		return "redirect:/list";
-	}
-	
-	//@GetMapping 어노테이션도 사용가능
-	@RequestMapping("/list")
-	public String list(Model model) {
-		
-		/*model.addAttribute("users", users);*/
-		//H2 데이터베이스 User 테이블에 있는 레코드를 
-		//모두 리스트로 출력 
-		model.addAttribute("users", 
-		userRepository.findAll());
-		
-		return "list";
-	}
-	
 }
